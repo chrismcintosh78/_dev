a00853447dff9005d2d9bf500117f3521fb37ccf
Chris McIntosh <cmcintosh.js@gmail.com>
2024-09-02 21:55:39 -0700

    =============== 0902242156 =============== Added necessary files for boilerplate startup
    - Included core classes (DB, Document, File, Template) in init.php
    - Updated public index.php to include init.php for loading necessary classes
    - Added a JavaScript redirect to public index.php

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php
+include_once("../core/classes/DB.php");
+include_once("../core/classes/Document.php");
+include_once("../core/classes/File.php");
+include_once("../core/classes/Template.php");
+?>
